a9e496df3dbd536874a34c88ad2bcc38a4aa9854
Nick Montesino <namontesino@gmail.com>
2020-11-10 16:12:42 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,3 @@
+puts "You know what year it is?"
+this_year = Time.now.year - 1
+puts "Hey, it's not #{this_year}" unless this_year == 2019
---
+++ practice.rb
@@ -0,0 +1,7 @@
+exit_num = 9
+count = 0 
+while count < 11 do 
+  break if count == 12
+  puts "I am the #{count}, I love to count"
+  count += 1
+end
